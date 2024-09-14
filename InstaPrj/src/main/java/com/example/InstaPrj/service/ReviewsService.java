038aecb1c34d3fef50b89d818af75a31aa5bdc58
이니 <38304918+JISUSAMA@users.noreply.github.com>
2024-09-14 22:31:39 +0900

    20240914

---
+++ InstaPrj/src/main/java/com/example/InstaPrj/service/ReviewsService.java
@@ -0,0 +1,40 @@
+package com.example.InstaPrj.service;
+import com.example.InstaPrj.dto.ReviewsDTO;
+import com.example.InstaPrj.entity.ClubMember;
+import com.example.InstaPrj.entity.Feeds;
+import com.example.InstaPrj.entity.Reviews;
+
+import java.util.List;
+
+public interface ReviewsService {
+  List<ReviewsDTO> getListOfFeeds(Long fno);
+  Long register(ReviewsDTO reviewsDTO);
+
+  void modify(ReviewsDTO reviewsDTO);
+
+  void remove(Long revieswnum);
+
+  public default Reviews dtoToEntity(ReviewsDTO reviewsDTO) {
+    Reviews review = Reviews.builder()
+        .reviewsnum(reviewsDTO.getReviewsnum())
+        .feeds(Feeds.builder().fno(reviewsDTO.getFno()).build())
+        .clubMember(ClubMember.builder().cno(reviewsDTO.getCno()).build())
+        .text(reviewsDTO.getText())
+        .build();
+    return review;
+  }
+
+  default ReviewsDTO entityToDto(Reviews reviews) {
+    ReviewsDTO reviewDTO = ReviewsDTO.builder()
+        .reviewsnum(reviews.getReviewsnum())
+        .fno(reviews.getFeeds().getFno())
+        .cno(reviews.getClubMember().getCno())
+        .nickname(reviews.getClubMember().getName())
+        .email(reviews.getClubMember().getEmail())
+        .text(reviews.getText())
+        .regDate(reviews.getRegDate())
+        .modDate(reviews.getModDate())
+        .build();
+    return reviewDTO;
+  }
+}
